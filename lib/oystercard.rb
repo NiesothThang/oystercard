e9fbe6ee7b2f0a35545d096ec0388b49f67b2467
Niesoth <niesoth123@gmail.com>
2021-06-08 15:35:17 +0100

    enabled top up

---
+++ lib/oystercard.rb
@@ -0,0 +1,9 @@
+class Oystercard
+    def initialize 
+    @balance = 0
+    end
+    def top_up(amount)
+    @balance += (amount)
+    end
+attr_reader :balance
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
+    it 'balance is equal to zero' do
+      expect(subject.balance).to eq 0
+    end
+    it 'expected to refelct top up balance' do
+        subject.top_up(10)
+      expect(subject.top_up(5)).to eq 15
+    end
+      
 end
